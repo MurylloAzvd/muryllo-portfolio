e208851fc070844c66d6fb7b4731994934276732
Muryllo <muryllo.dev@gmail.com>
2022-08-19 21:40:25 -0300

    add loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,44 @@
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import Aos from "aos";
-import { ThemeProvider, DefaultTheme } from "styled-components";
+import { Router } from "next/router";
+import NProgress from "nprogress";
+
 import { Footer } from "../components/FooterSection";
 import { Menu } from "../components/Menu";
 import { ScrollToTopButton } from "../components/ScrollToTopButton";
+
 import GlobalStyle from "../styles/globalstyles";
 import "aos/dist/aos.css";
-
-const theme: DefaultTheme = {
-  colors: {
-    primary: "#111",
-    secondary: "#0070f3",
-  },
-};
+import "nprogress/nprogress.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     Aos.init({ once: true });
   }, []);
 
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Menu />
-        <Component {...pageProps} />
-        <Footer />
-        <ScrollToTopButton />
-      </ThemeProvider>
+      <GlobalStyle />
+      <Menu />
+      <Component {...pageProps} />
+      <Footer />
+      <ScrollToTopButton />
     </>
   );
 }
