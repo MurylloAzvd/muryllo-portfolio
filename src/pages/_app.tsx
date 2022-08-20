40133d0e5a1858d15b753db1cf337f28c037e7a8
Muryllo <muryllo.dev@gmail.com>
2022-08-20 19:42:52 -0300

    add seo tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import Aos from "aos";
 import { Router } from "next/router";
 import NProgress from "nprogress";
+import useTranslation from "next-translate/useTranslation";
+import Head from "next/head";
 
 import { Footer } from "../components/FooterSection";
 import { Menu } from "../components/Menu";
@@ -13,6 +15,8 @@ import "aos/dist/aos.css";
 import "nprogress/nprogress.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { t } = useTranslation("common");
+
   useEffect(() => {
     Aos.init({ once: true });
   }, []);
@@ -34,6 +38,29 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <title>{t("page-title")}</title>
+        <meta name="description" content={t("page-description")} />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest"></link>
+      </Head>
       <GlobalStyle />
       <Menu />
       <Component {...pageProps} />
